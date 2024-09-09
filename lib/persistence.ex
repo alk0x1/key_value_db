992d866cfff6b830c629fef94fcb05eecfdb6f3a
Alan Bertani <alanbertani051@gmail.com>
2024-09-08 22:11:00 -0300

    replace msgpax to native erlang serializer

--- lib/persistence.ex
+++ lib/persistence.ex
@@ -2,11 +2,11 @@ defmodule Persistence do
   @file_path "state.bin"      # Compacted state file (snapshot)
   @tmp_file_path "state.tmp"  # Temp file for atomic operations
   @log_file "state.log"       # Log file to record all changes
+  @separator "\n"
 
   def save_full_map(%{stack: [first_map | _]}) do
-    {:ok, binary} = Msgpax.pack(first_map)
+    binary = :erlang.term_to_binary(first_map)
 
-    # Write the snapshot (compacted) state to a temp file, then rename
     File.write!(@tmp_file_path, binary)
     File.rename(@tmp_file_path, @file_path)
   end
@@ -14,7 +14,7 @@ defmodule Persistence do
   def load_full_map do
     case File.read(@file_path) do
       {:ok, binary} ->
-        {:ok, first_map} = Msgpax.unpack(binary)
+        first_map = :erlang.binary_to_term(binary)
         first_map
 
       {:error, :enoent} ->
@@ -24,8 +24,8 @@ defmodule Persistence do
 
   # Append incremental changes to the log file (serialize as a list)
   def log_change({key, value}) do
-    log_entry = Msgpax.pack!([key, value])
-    File.write!(@log_file, log_entry, [:append])
+    log_entry = :erlang.term_to_binary([key, value])
+    File.write!(@log_file, log_entry <> @separator, [:append])
   end
 
   # Replay the log to rebuild the map state from the log entries
@@ -43,16 +43,10 @@ defmodule Persistence do
     end
   end
 
-  def extract_log_entries(binary_data) do
-    do_extract_log_entries(binary_data, [])
-  end
-
-  def do_extract_log_entries("", acc), do: Enum.reverse(acc)
-
-  def do_extract_log_entries(binary_data, acc) do
-    # Unpack one log entry from the start of the binay
-    {log_entry, rest} = Msgpax.unpack_slice!(binary_data)
-    do_extract_log_entries(rest, [log_entry | acc])
+  def extract_log_entries(log_data) do
+    log_data
+    |> String.split(@separator, trim: true)
+    |> Enum.map(& :erlang.binary_to_term/1)
   end
 
   def compact_log(%{stack: [_first_map | _]} = state) do
